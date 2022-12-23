03d9c633a25f68846a3ebbd640451b1913c8fd3b
¨valeria <valeria.zimmermann16@gmail.com>
2022-12-23 13:10:51 -0300

    ÂEntrega1

---
+++ Desktop/React/entrega1/src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/Navbar";
+import ItemListContainer from './components/ItemListContainer';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar/>
+      <ItemListContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/React/entrega1/src/components/CartWidget/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BsCartFill} from "react-icons/bs";
+
+
+const CartWidget = () => {
+    return (
+            <BsCartFill style={{height:20, width:30}}/>
+        
+    )
+};
+
+export default CartWidget;
